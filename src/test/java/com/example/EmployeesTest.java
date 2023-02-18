4e02b70eb752263776321d5b9647fe183309c7e9
Hanna Friberg <friberg.hanna@outlook.com>
2023-02-18 17:25:46 +0100

    Added test for findAll();

---
+++ src/test/java/com/example/EmployeesTest.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+
+public class EmployeesTest {
+    EmployeeRepositoryStub employeeRepositoryStub = new EmployeeRepositoryStub();
+    BankService bankService = new BankServiceDummy();
+    Employees employees = new Employees(employeeRepositoryStub, bankService);
+
+    @Test
+    void testFindAllShouldReturnOne() {
+        EmployeeRepository employeeRepository = mock(EmployeeRepository.class);
+        Assertions.assertNotSame(employeeRepository.findAll(),employeeRepositoryStub.findAll());
+
+    }
+}
